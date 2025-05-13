8032fb0de1989fb2a9b12a18edf1a79d027e1677
Francisco <franciscoocontatoo@gmail.com>
2025-05-13 14:17:07 -0300

    Enviando lista de Exercícios C#

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; // Pacotes - Mesma ideia de bibliotecas (Python; Golang utiliza os packages também)
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Project name
+namespace HelloWorld{
+
+    // Class 
+    class Program {
+        
+        // Main function 
+        static void Main(string[] args)
+        {
+            // This is a comment 
+            /*
+             This is also a comment
+             */
+
+            dynamic valorDinamico = "Blue";
+            
+            Console.WriteLine(valorDinamico);
+
+        }
+    }
+}
